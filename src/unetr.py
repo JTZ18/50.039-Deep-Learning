faefb1fa449aa80b24a7764e650544a086430766
chuanshaof <chuanshaof@gmail.com>
2024-04-09 14:18:57 +0800

    unetr model completed

--- src/unetr.py
+++ src/unetr.py
@@ -2,13 +2,17 @@ import torch.nn as nn
 import torch
 from src.utils.vit import ViT
 
-""" Adapted from: https://paperswithcode.com/method/unetr """
+""" 
+    Adapted from: 
+        https://github.com/tamasino52/UNETR/blob/main/unetr.py
+        https://github.com/Project-MONAI/research-contributions/tree/main/UNETR/BTCV 
+"""
 
-class Conv2DBlock(nn.Module):
-    def __init__(self, in_planes, out_planes, kernel_size=3):
+class YellowBlock(nn.Module):
+    def __init__(self, in_planes, out_planes):
         super().__init__()
         self.block = nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=1, padding=((kernel_size - 1) // 2)),
+            nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_planes),
             nn.ReLU(True)
         )
@@ -17,12 +21,12 @@ class Conv2DBlock(nn.Module):
         return self.block(x)
 
 
-class Deconv2DBlock(nn.Module):
-    def __init__(self, in_planes, out_planes, kernel_size=3):
+class BlueBlock(nn.Module):
+    def __init__(self, in_planes, out_planes):
         super().__init__()
         self.block = nn.Sequential(
             nn.ConvTranspose2d(in_planes, out_planes, kernel_size=2, stride=2, padding=0, output_padding=0), 
-            nn.Conv2d(out_planes, out_planes, kernel_size=kernel_size, stride=1, padding=((kernel_size - 1) // 2)),
+            nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_planes),
             nn.ReLU(True)
         )
@@ -30,6 +34,14 @@ class Deconv2DBlock(nn.Module):
     def forward(self, x):
         return self.block(x)
 
+
+class GreenBlock(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super().__init__()
+        self.block = nn.ConvTranspose2d(in_planes, out_planes, kernel_size=2, stride=2, padding=0, output_padding=0)
+
+    def forward(self, x):
+        return self.block(x)
 
 class UNETR(nn.Module):
     def __init__(self, img_shape=(256, 256), input_dim=3, output_dim=1, embed_dim=768, patch_size=16, num_heads=12, dropout=0.1):
@@ -51,7 +63,6 @@ class UNETR(nn.Module):
         self.num_heads = num_heads
         self.dropout = dropout
         self.num_layers = 12
-        self.ext_layers = [3, 6, 9, 12]
 
         self.patch_dim = [int(x / patch_size) for x in img_shape]
 
@@ -62,83 +73,83 @@ class UNETR(nn.Module):
             patch_size,
             num_heads,
             self.num_layers,
-            dropout,
-            self.ext_layers
+            dropout
         )
 
-        # Very top, just connecting the original with output
-        self.yellow_decoder_x = \
+        # NOTE: This follows the U-shape that goes down then up
+        self.yellow_encoder_x = \
             nn.Sequential(
-                Conv2DBlock(input_dim, 32, 3),
-                Conv2DBlock(32, 64, 3)
+                YellowBlock(input_dim, 32),
+                YellowBlock(32, 64)
             )
         
-        self.blue_decoder_z3 = \
+        self.blue_encoder_z3 = \
             nn.Sequential(
-                Deconv2DBlock(embed_dim, 512),
-                Deconv2DBlock(512, 256),
-                Deconv2DBlock(256, 128)
+                BlueBlock(embed_dim, 512),
+                BlueBlock(512, 256),
+                BlueBlock(256, 128)
             )
         
-        self.blue_decoder_z6 = \
+        self.blue_encoder_z6 = \
             nn.Sequential(
-                Deconv2DBlock(embed_dim, 512),
-                Deconv2DBlock(512, 256),
+                BlueBlock(embed_dim, 512),
+                BlueBlock(512, 256),
             )
         
-        self.blue_decoder_z9 = Deconv2DBlock(embed_dim, 512)
+        self.blue_encoder_z9 = BlueBlock(embed_dim, 512)
         
-        self.green_upsampler_z3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2, padding=0, output_padding=0)
-        self.green_upsampler_z6 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2, padding=0, output_padding=0)
-        self.green_upsampler_z9 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2, padding=0, output_padding=0)
-        self.green_upsampler_z12 = nn.ConvTranspose2d(embed_dim, 512, kernel_size=2, stride=2, padding=0, output_padding=0)
+        self.green_upsampler_z12 = GreenBlock(embed_dim, 512)
 
-        self.yellow_decoder_z0 = \
-            nn.Sequential(
-                Conv2DBlock(128, 64),
-                Conv2DBlock(64, 64),
-            )
-        self.yellow_decoder_z3 = \
-            nn.Sequential(
-                Conv2DBlock(256, 128),
-                Conv2DBlock(128, 128),
-            )
-        self.yellow_decoder_z6 = \
-            nn.Sequential(
-                Conv2DBlock(512, 256),
-                Conv2DBlock(256, 256),
-            )
         self.yellow_decoder_z9 = \
             nn.Sequential(
-                Conv2DBlock(1024, 512),
-                Conv2DBlock(512, 512),
+                YellowBlock(1024, 512),
+                YellowBlock(512, 512),
             )
-
+        self.green_upsampler_z9 = GreenBlock(512, 256)
+        
+        self.yellow_decoder_z6 = \
+            nn.Sequential(
+                YellowBlock(512, 256),
+                YellowBlock(256, 256),
+            )
+        self.green_upsampler_z6 = GreenBlock(256, 128)
+        
+        self.yellow_decoder_z3 = \
+            nn.Sequential(
+                YellowBlock(256, 128),
+                YellowBlock(128, 128),
+            )        
+        self.green_upsampler_z3 = GreenBlock(128, 64)
+        
+        self.yellow_decoder_z0 = \
+            nn.Sequential(
+                YellowBlock(128, 64),
+                YellowBlock(64, 64),
+            )
         self.final_conv = nn.Conv2d(64, output_dim, kernel_size=1, stride=1, padding=0)
         
     def forward(self, x):
-        z = self.transformer(x)
-        z0, z3, z6, z9, z12 = x, *z
-        z3 = z3.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
-        z6 = z6.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
-        z9 = z9.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
-        z12 = z12.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
+        z3, z6, z9, z12 = self.transformer(x)
 
+        z12 = z12.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
         z12 = self.green_upsampler_z12(z12)
 
-        z9 = self.blue_decoder_z9(z9)
+        z9 = z9.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
+        z9 = self.blue_encoder_z9(z9)
         z9 = self.yellow_decoder_z9(torch.cat([z9, z12], dim=1))
         z9 = self.green_upsampler_z9(z9)
 
-        z6 = self.blue_decoder_z6(z6)
+        z6 = z6.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
+        z6 = self.blue_encoder_z6(z6)
         z6 = self.yellow_decoder_z6(torch.cat([z6, z9], dim=1))
         z6 = self.green_upsampler_z6(z6)
 
-        z3 = self.blue_decoder_z3(z3)
+        z3 = z3.transpose(-1, -2).view(-1, self.embed_dim, *self.patch_dim)
+        z3 = self.blue_encoder_z3(z3)
         z3 = self.yellow_decoder_z3(torch.cat([z3, z6], dim=1))
         z3 = self.green_upsampler_z3(z3)
 
-        z0 = self.yellow_decoder_x(z0)
+        z0 = self.yellow_encoder_x(x)
         z0 = self.yellow_decoder_z0(torch.cat([z0, z3], dim=1))
         
         output = self.final_conv(z0)
--- src/utils/vit.py
+++ src/utils/vit.py
@@ -1,89 +1,28 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-import copy
-import math
+from einops.layers.torch import Rearrange
 
-class SelfAttention(nn.Module):
-    def __init__(self, num_heads, embed_dim, dropout):
-        super().__init__()
-        self.num_attention_heads = num_heads
-        self.attention_head_size = int(embed_dim / num_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
-
-        self.query = nn.Linear(embed_dim, self.all_head_size)
-        self.key = nn.Linear(embed_dim, self.all_head_size)
-        self.value = nn.Linear(embed_dim, self.all_head_size)
-
-        self.out = nn.Linear(embed_dim, embed_dim)
-        self.attn_dropout = nn.Dropout(dropout)
-        self.proj_dropout = nn.Dropout(dropout)
-
-        self.softmax = nn.Softmax(dim=-1)
-
-        self.vis = False
-
-    def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-        x = x.view(*new_x_shape)
-        return x.permute(0, 2, 1, 3)
-
-    def forward(self, hidden_states):
-        mixed_query_layer = self.query(hidden_states)
-        mixed_key_layer = self.key(hidden_states)
-        mixed_value_layer = self.value(hidden_states)
-
-        query_layer = self.transpose_for_scores(mixed_query_layer)
-        key_layer = self.transpose_for_scores(mixed_key_layer)
-        value_layer = self.transpose_for_scores(mixed_value_layer)
-
-        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-        attention_probs = self.softmax(attention_scores)
-        weights = attention_probs if self.vis else None
-        attention_probs = self.attn_dropout(attention_probs)
-
-        context_layer = torch.matmul(attention_probs, value_layer)
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
-        attention_output = self.out(context_layer)
-        attention_output = self.proj_dropout(attention_output)
-        return attention_output, weights
-
-
-class Mlp(nn.Module):
-    def __init__(self, in_features, act_layer=nn.GELU, drop=0.):
-        super().__init__()
-        self.fc1 = nn.Linear(in_features, in_features)
-        self.act = act_layer()
-        self.drop = nn.Dropout(drop)
-
-    def forward(self, x):
-        x = self.fc1()
-        x = self.act(x)
-        x = self.drop(x)
-        return x
-
-
-class PositionwiseFeedForward(nn.Module):
-    def __init__(self, d_model=786, d_ff=2048, dropout=0.1):
-        super().__init__()
-        # Torch linears have a `b` by default.
-        self.w_1 = nn.Linear(d_model, d_ff)
-        self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        return self.w_2(self.dropout(F.relu(self.w_1(x))))
-
+""" 
+    Adapted from: 
+        https://github.com/tamasino52/UNETR/blob/main/unetr.py
+        https://github.com/Project-MONAI/research-contributions/tree/main/UNETR/BTCV 
+"""
 
 class Embeddings(nn.Module):
+    """ Convolutional positional embeddings for the image """
     def __init__(self, input_dim, embed_dim, img_size, patch_size, dropout):
+        """
+            input_dim: Number of color channels
+            embed_dim: Embedding dimension
+            img_size: Dimension of image
+            patch_size: Patch size
+            dropout: Dropout rate
+        """
         super().__init__()
-        self.n_patches = int((img_size[0] * img_size[1]) / (patch_size * patch_size))
         self.patch_size = patch_size
         self.embed_dim = embed_dim
+        self.n_patches = int((img_size[0] * img_size[1]) / (patch_size * patch_size))
         self.patch_embeddings = nn.Conv2d(in_channels=input_dim, out_channels=embed_dim,
                                           kernel_size=patch_size, stride=patch_size)
         self.position_embeddings = nn.Parameter(torch.zeros(1, self.n_patches, embed_dim))
@@ -91,54 +30,99 @@ class Embeddings(nn.Module):
 
     def forward(self, x):
         x = self.patch_embeddings(x)
-        x = x.flatten(2)
-        x = x.transpose(-1, -2)
+        x = x.flatten(2).transpose(-1, -2)
         embeddings = x + self.position_embeddings
         embeddings = self.dropout(embeddings)
         return embeddings
-
-
-class TransformerBlock(nn.Module):
-    def __init__(self, embed_dim, num_heads, dropout, img_size, patch_size):
+    
+class SelfAttention(nn.Module):
+    """ Self-attention Block """
+    def __init__(self, num_heads, embed_dim, dropout):
+        """
+            num_heads: Number of attention heads
+            embed_dim: Embedding dimension
+            dropout: Dropout rate
+        """
         super().__init__()
-        self.attention_norm = nn.LayerNorm(embed_dim, eps=1e-6)
-        self.mlp_norm = nn.LayerNorm(embed_dim, eps=1e-6)
-        self.mlp_dim = int((img_size[0] * img_size[1]) / (patch_size * patch_size))
-        self.mlp = PositionwiseFeedForward(embed_dim, 2048)
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        
+        self.qkv = nn.Linear(embed_dim, embed_dim * 3, bias=False)
+        self.input_rearrange = Rearrange("b h (qkv l d) -> qkv b l h d", qkv=3, l=num_heads)
+        self.out_rearrange = Rearrange("b h l d -> b l (h d)")
+
+        self.attn_dropout = nn.Dropout(dropout)
+        self.proj_dropout = nn.Dropout(dropout)
+
+        self.out_proj = nn.Linear(embed_dim, embed_dim)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        qkv = self.input_rearrange(self.qkv(x))
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        att_mat = (torch.einsum("blxd,blyd->blxy", q, k) * (self.head_dim**-0.5))
+        att_mat = self.attn_dropout(self.softmax(att_mat))
+        x = torch.einsum("blxy,blyd->blxd", att_mat, v)
+        x = self.out_rearrange(x)
+        x = self.proj_dropout(self.out_proj(x))
+        return x
+    
+
+class PositionwiseFeedForward(nn.Module):
+    "" "Implements FFN equation, commonly found everywhere """
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+    
+class TransformerBlock(nn.Module):
+    """ Transformer Block """
+    def __init__(self, embed_dim, num_heads, dropout, img_size, patch_size):
+        """
+            embed_dim: Embedding dimension
+            num_heads: Number of attention heads
+            dropout: Dropout rate
+            img_size: Dimension of image
+            patch_size: Patch size
+        """
+        super().__init__()
+        self.attn_norm = nn.LayerNorm(embed_dim, eps=1e-6)
         self.attn = SelfAttention(num_heads, embed_dim, dropout)
 
+        self.mlp_norm = nn.LayerNorm(embed_dim, eps=1e-6)
+        self.mlp = PositionwiseFeedForward(embed_dim, 2048) # Based on the original paper
+
     def forward(self, x):
-        h = x
-        x = self.attention_norm(x)
-        x, weights = self.attn(x)
-        x = x + h
-        h = x
-
-        x = self.mlp_norm(x)
-        x = self.mlp(x)
-
-        x = x + h
-        return x, weights
-
-
+        x = x + self.attn(self.attn_norm(x))
+        x = x + self.mlp(self.mlp_norm(x))
+        return x
+    
 class ViT(nn.Module):
-    def __init__(self, input_dim, embed_dim, img_size, patch_size, num_heads, num_layers, dropout, extract_layers):
+    """ Vision Transformer modified to extract hidden states from specified layers """
+    def __init__(self, input_dim, embed_dim, img_size, patch_size, num_heads, num_layers, dropout):
         super().__init__()
         self.embeddings = Embeddings(input_dim, embed_dim, img_size, patch_size, dropout)
-        self.layer = nn.ModuleList()
-        self.encoder_norm = nn.LayerNorm(embed_dim, eps=1e-6)
-        self.extract_layers = extract_layers
-        for _ in range(num_layers):
-            layer = TransformerBlock(embed_dim, num_heads, dropout, img_size, patch_size)
-            self.layer.append(copy.deepcopy(layer))
+
+        self.blocks = nn.ModuleList(
+            [
+                TransformerBlock(embed_dim, num_heads, dropout, img_size, patch_size)
+                for i in range(num_layers)
+            ]
+        )
+
+        self.norm = nn.LayerNorm(embed_dim, eps=1e-6)
 
     def forward(self, x):
+        x = self.embeddings(x)
         extract_layers = []
-        hidden_states = self.embeddings(x)
 
-        for depth, layer_block in enumerate(self.layer):
-            hidden_states, _ = layer_block(hidden_states)
-            if depth + 1 in self.extract_layers:
-                extract_layers.append(hidden_states)
+        for block in self.blocks:
+            x = block(x)
+            extract_layers.append(x)
 
-        return extract_layers
+        x = self.norm(x)
+        return extract_layers[2], extract_layers[5], extract_layers[8], x
