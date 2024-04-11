58ea819f264a45fba42cb713a7cb7730f8f7aeb2
chuanshaof <chuanshaof@gmail.com>
2024-04-11 22:12:30 +0800

    fixed to work with different kernels

--- src/unet.py
+++ src/unet.py
@@ -5,12 +5,13 @@ import torchvision.transforms.functional as TF
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, dropout_rate=0.1):
         super(DoubleConv, self).__init__()
+        padding = kernel_size // 2
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, 1, 1, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size, 1, padding, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Dropout(dropout_rate),
-            nn.Conv2d(out_channels, out_channels, kernel_size, 1, 1, bias=False),
+            nn.Conv2d(out_channels, out_channels, kernel_size, 1, padding, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Dropout(dropout_rate)
