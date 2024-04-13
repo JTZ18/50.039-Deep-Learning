dea53fc80edc96a50e3373df0aefa5e13ed55fa3
JTZ18 <jon.taylorlim99@gmail.com>
2024-04-14 02:54:14 +0800

    refactor segformer filename to use updated segformer

--- src/segFormer.py
+++ src/segFormer.py
@@ -1,262 +1,321 @@
-"""
-References: https://github.com/lucidrains/segformer-pytorch
-"""
-from math import sqrt
-from functools import partial
 import torch
-from torch import nn, einsum
-import torch.nn.functional as F
-
 from einops import rearrange
+from torch import nn
+from torchvision.ops import StochasticDepth
+from typing import List, Iterable
 
-# helpers
-
-def exists(val):
-    return val is not None
-
-def cast_tuple(val, depth):
-    return val if isinstance(val, tuple) else (val,) * depth
-
-# classes
-
-class DsConv2d(nn.Module):
-    """
-    This class is a depthwise separable convolution module.
-    It performs a depthwise convolution followed by a pointwise convolution.
-    This is a more efficient form of convolution operation.
-    """
-    def __init__(self, dim_in, dim_out, kernel_size, padding, stride = 1, bias = True):
+class LayerNorm2d(nn.Module):
+    """ Layer Norm with channel swapping and support """
+    def __init__(self, channels: int):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(dim_in, dim_in, kernel_size = kernel_size, padding = padding, groups = dim_in, stride = stride, bias = bias),
-            nn.Conv2d(dim_in, dim_out, kernel_size = 1, bias = bias)
-        )
-    def forward(self, x):
-        return self.net(x)
-
-class LayerNorm(nn.Module):
-    """
-    This class is a layer normalization module.
-    It normalizes the features across the channel dimension (as opposed to batch normalization which normalizes features across the batch dimension).
-    """
-    def __init__(self, dim, eps = 1e-5):
-        super().__init__()
-        self.eps = eps
-        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
-        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
+        self.ln = nn.LayerNorm(channels)
 
     def forward(self, x):
-        std = torch.var(x, dim = 1, unbiased = False, keepdim = True).sqrt()
-        mean = torch.mean(x, dim = 1, keepdim = True)
-        return (x - mean) / (std + self.eps) * self.g + self.b
-
-class PreNorm(nn.Module):
-    """
-    This class applies layer normalization before calling the provided function fn.
-    This is a common technique in transformer architectures.
-    """
-    def __init__(self, dim, fn):
+        x = rearrange(x, "b c h w -> b h w c")
+        x = self.ln(x)
+        x = rearrange(x, "b h w c -> b c h w")
+        return x
+    
+class ResidualAdd(nn.Module):
+    """Just an util layer"""
+    def __init__(self, fn):
         super().__init__()
         self.fn = fn
-        self.norm = LayerNorm(dim)
 
-    def forward(self, x):
-        return self.fn(self.norm(x))
+    def forward(self, x, **kwargs):
+        out = self.fn(x, **kwargs)
+        x = x + out
+        return x
 
-class EfficientSelfAttention(nn.Module):
-    """
-    This class is a self-attention module.
-    It computes the attention scores for each element in the input based on its interaction with all other elements.
-    The "efficient" part comes from the fact that it uses a more memory-efficient way of computing the attention scores.
-    """
-    def __init__(
-        self,
-        *,
-        dim,
-        heads,
-        reduction_ratio
-    ):
+class OverlapPatchMerging(nn.Module):
+    """ Overlapping patch merging layer """
+    def __init__(self, in_channels: int, out_channels: int, patch_size: int, overlap_size: int):
         super().__init__()
-        self.scale = (dim // heads) ** -0.5
-        self.heads = heads
-
-        self.to_q = nn.Conv2d(dim, dim, 1, bias = False)
-        self.to_kv = nn.Conv2d(dim, dim * 2, reduction_ratio, stride = reduction_ratio, bias = False)
-        self.to_out = nn.Conv2d(dim, dim, 1, bias = False)
-
-    def forward(self, x):
-        h, w = x.shape[-2:]
-        heads = self.heads
-
-        q, k, v = (self.to_q(x), *self.to_kv(x).chunk(2, dim = 1))
-        q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> (b h) (x y) c', h = heads), (q, k, v))
-
-        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
-        attn = sim.softmax(dim = -1)
-
-        out = einsum('b i j, b j d -> b i d', attn, v)
-        out = rearrange(out, '(b h) (x y) c -> b (h c) x y', h = heads, x = h, y = w)
-        return self.to_out(out)
-
-class MixFeedForward(nn.Module):
-    """
-    This class is a feed-forward network module.
-    It consists of two linear transformations with a GELU activation in between.
-    The first linear transformation expands the dimensionality of the input (for increased model capacity), and the second one projects it back to the original dimensionality.
-    """
-    def __init__(
-        self,
-        *,
-        dim,
-        expansion_factor,
-        dropout=0.1
-    ):
-        super().__init__()
-        hidden_dim = dim * expansion_factor
-        self.net = nn.Sequential(
-            nn.Conv2d(dim, hidden_dim, 1),
-            DsConv2d(hidden_dim, hidden_dim, 3, padding = 1),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Conv2d(hidden_dim, dim, 1)
+        self.block = nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=patch_size,
+                stride=overlap_size,
+                padding=patch_size // 2,
+                bias=False
+            ),
+            LayerNorm2d(out_channels)
         )
 
     def forward(self, x):
-        return self.net(x)
+        return self.block(x)
+
+class EfficientMultiHeadAttention(nn.Module):
+    """ Efficient Multi Head Attention Layer """
+    def __init__(self, channels: int, reduction_ratio: int = 1, num_heads: int = 8):
+        super().__init__()
+        self.reducer = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size=reduction_ratio, stride=reduction_ratio),
+            LayerNorm2d(channels),
+        )
+        self.attn = nn.MultiheadAttention(channels, num_heads=num_heads, batch_first=True)
+
+    def forward(self, x):
+        _, _, h, w = x.shape
+        reduced_x = self.reducer(x)
+
+        reduced_x = rearrange(reduced_x, "b c h w -> b (h w) c")
+        x = rearrange(x, "b c h w -> b (h w) c")
+        out = self.attn(x, reduced_x, reduced_x)[0]
+
+        out = rearrange(out, "b (h w) c -> b c h w", h=h, w=w)
+        return out
+
+class MixFNN(nn.Module):
+    """ Mix MLP Layer """
+    def __init__(self, channels: int, expansion: int = 4):
+        super().__init__()
+        self.mlp1 = nn.Conv2d(channels, channels, kernel_size=1)
+        self.conv3 = nn.Conv2d(
+            channels,
+            channels * expansion,
+            kernel_size=3,
+            groups=channels,
+            padding=1,
+        )
+        self.gelu = nn.GELU()
+        self.mlp2 = nn.Conv2d(channels * expansion, channels, kernel_size=1)
+    
+    def forward(self, x):
+        x = self.mlp1(x)
+        x = self.conv3(x)
+        x = self.gelu(x)
+        x = self.mlp2(x)
+        return x
 
 class MiT(nn.Module):
-    """
-    This class is the main transformer module.
-    It applies a sequence of transformations to the input, including extracting overlapping patches, embedding the patches, and applying a series of transformer layers.
-    """
+    """ MiT Block includign Efficient Self-Attention and Mix-FFN """
     def __init__(
         self,
-        *,
-        channels,
-        dims,
-        heads,
-        ff_expansion,
-        reduction_ratio,
-        num_layers,
-        dropout
+        channels: int,
+        reduction_ratio: int = 1,
+        num_heads: int = 8,
+        mlp_expansion: int = 4,
+        dropout: float = .0
     ):
         super().__init__()
-        stage_kernel_stride_pad = ((7, 4, 3), (3, 2, 1), (3, 2, 1), (3, 2, 1))
-
-        dims = (channels, *dims)
-        dim_pairs = list(zip(dims[:-1], dims[1:]))
-
-        self.stages = nn.ModuleList([])
-
-        for (dim_in, dim_out), (kernel, stride, padding), num_layers, ff_expansion, heads, reduction_ratio in zip(dim_pairs, stage_kernel_stride_pad, num_layers, ff_expansion, heads, reduction_ratio):
-            get_overlap_patches = nn.Unfold(kernel, stride = stride, padding = padding)
-            overlap_patch_embed = nn.Conv2d(dim_in * kernel ** 2, dim_out, 1)
-
-            layers = nn.ModuleList([])
-
-            for _ in range(num_layers):
-                layers.append(nn.ModuleList([
-                    PreNorm(dim_out, EfficientSelfAttention(dim = dim_out, heads = heads, reduction_ratio = reduction_ratio)),
-                    PreNorm(dim_out, MixFeedForward(dim = dim_out, expansion_factor = ff_expansion)),
-                ]))
-
-            self.stages.append(nn.ModuleList([
-                get_overlap_patches,
-                overlap_patch_embed,
-                layers
-            ]))
-
-    def forward(
-        self,
-        x,
-        return_layer_outputs = False
-    ):
-        h, w = x.shape[-2:]
-
-        layer_outputs = []
-        for (get_overlap_patches, overlap_embed, layers) in self.stages:
-            x = get_overlap_patches(x)
-
-            num_patches = x.shape[-1]
-            ratio = int(sqrt((h * w) / num_patches))
-            x = rearrange(x, 'b c (h w) -> b c h w', h = h // ratio)
-
-            x = overlap_embed(x)
-            for (attn, ff) in layers:
-                x = attn(x) + x
-                x = ff(x) + x
-
-            layer_outputs.append(x)
-
-        ret = x if not return_layer_outputs else layer_outputs
-        return ret
-
-class Segformer(nn.Module):
-    """
-    This is the main model class.
-    It first applies the MiT transformer to the input, then fuses the outputs of the transformer layers,
-    and finally applies a segmentation head to the fused output to produce the final segmentation map.
-    """
-    def __init__(
-        self,
-        *,
-        dims = (32, 64, 160, 256),
-        heads = (1, 2, 5, 8),
-        ff_expansion = (8, 8, 4, 4),
-        reduction_ratio = (8, 4, 2, 1),
-        num_layers = 2,
-        channels = 3,
-        decoder_dim = 256,
-        num_classes = 1,
-        dropout = 0.1
-    ):
-        super().__init__()
-        dims, heads, ff_expansion, reduction_ratio, num_layers = map(partial(cast_tuple, depth = 4), (dims, heads, ff_expansion, reduction_ratio, num_layers))
-        assert all([*map(lambda t: len(t) == 4, (dims, heads, ff_expansion, reduction_ratio, num_layers))]), 'only four stages are allowed, all keyword arguments must be either a single value or a tuple of 4 values'
-
-        self.mit = MiT(
-            channels = channels,
-            dims = dims,
-            heads = heads,
-            ff_expansion = ff_expansion,
-            reduction_ratio = reduction_ratio,
-            num_layers = num_layers,
-            dropout = dropout
+        self.attn = nn.Sequential(
+            LayerNorm2d(channels),
+            EfficientMultiHeadAttention(channels, reduction_ratio, num_heads),
+            nn.Dropout(dropout)
         )
 
-        self.to_fused = nn.ModuleList([nn.Sequential(
-            nn.Conv2d(dim, decoder_dim, 1),
-            nn.Upsample(scale_factor = 2 ** i)
-        ) for i, dim in enumerate(dims)])
+        self.mix = nn.Sequential(
+            LayerNorm2d(channels),
+            MixFNN(channels, expansion=mlp_expansion),
+            nn.Dropout(dropout)
+        )
+    
+    def forward(self, x):
+        x = x + self.attn(x)
+        x = x + self.mix(x)
+        return x
 
-        self.to_segmentation = nn.Sequential(
-            nn.Conv2d(4 * decoder_dim, decoder_dim, 1),
-            nn.Dropout(dropout),
-            nn.Conv2d(decoder_dim, num_classes, 1),
+class TransformerBlock(nn.Module):
+    """ Orange Transformer Block """
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        patch_size: int,
+        overlap_size: int,
+        dropout: List[int],
+        depth: int = 2,
+        reduction_ratio: int = 1,
+        num_heads: int = 8,
+        mlp_expansion: int = 4,
+    ):
+        super().__init__()
+        self.overlap_patch_merge = OverlapPatchMerging(in_channels, out_channels, patch_size, overlap_size)
+        self.mit = nn.ModuleList(
+            [
+                MiT(
+                    out_channels, reduction_ratio, num_heads, mlp_expansion, dropout[i]
+                )
+                for i in range(depth)
+            ]
+        )
+        self.norm = LayerNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.overlap_patch_merge(x)
+        for mit in self.mit:
+            x = mit(x)
+        x = self.norm(x)
+        return x
+
+def chunks(data: Iterable, sizes: List[int]):
+    """
+    Given an iterable, returns slices using sizes as indices
+    """
+    curr = 0
+    for size in sizes:
+        chunk = data[curr: curr + size]
+        curr += size
+        yield chunk
+
+class Encoder(nn.Module):
+    """ Encoder (Front) portion """
+    def __init__(
+        self,
+        in_channels: int,
+        widths: List[int],
+        depths: List[int],
+        all_num_heads: List[int],
+        patch_sizes: List[int],
+        overlap_sizes: List[int],
+        reduction_ratios: List[int],
+        mlp_expansions: List[int],
+        dropout: float = .0
+    ):
+        super().__init__()
+        dropout =  [x.item() for x in torch.linspace(0, dropout, sum(depths))]
+        self.stages = nn.ModuleList(
+            [
+                TransformerBlock(*args)
+                for args in zip(
+                    [in_channels, *widths],
+                    widths,
+                    patch_sizes,
+                    overlap_sizes,
+                    chunks(dropout, sizes=depths),
+                    depths,
+                    reduction_ratios,
+                    all_num_heads,
+                    mlp_expansions
+                )
+            ]
         )
 
     def forward(self, x):
-        layer_outputs = self.mit(x, return_layer_outputs = True)
+        features = []
+        for stage in self.stages:
+            x = stage(x)
+            features.append(x)
+        return features
 
-        fused = [to_fused(output) for output, to_fused in zip(layer_outputs, self.to_fused)]
-        fused = torch.cat(fused, dim = 1)
-        seg_output = self.to_segmentation(fused)
+
+class MLPLayer(nn.Module):
+    """ Gray MLPLayer in decoder """
+    def __init__(self, in_channels: int, out_channels: int, scale_factor: int = 2):
+        super().__init__()
+        self.mlp = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.upsample = nn.UpsamplingBilinear2d(scale_factor=scale_factor)
+
+    def forward(self, x):
+        return self.upsample(self.mlp(x))
+
+class Decoder(nn.Module):
+    """ Decoder (Back) portion """
+    def __init__(self, out_channels: int, widths: List[int], scale_factors: List[int]):
+        super().__init__()
+        self.stages = nn.ModuleList(
+            [
+                MLPLayer(in_channels, out_channels, scale_factor)
+                for in_channels, scale_factor in zip(widths, scale_factors)
+            ]
+        )
+
+    def forward(self, features):
+        new_features = []
+        for feature, stage in zip(features, self.stages):
+            x = stage(feature)
+            new_features.append(x)
+        return new_features
+
+class SegmentationHead(nn.Module):
+    """ Output segmentation head, blue MLP at the back """
+    def __init__(self, channels: int, num_classes: int, num_features: int = 4):
+        super().__init__()
+        # Fuse 4 channels into 1
+        self.fusion = nn.Sequential(
+            nn.Conv2d(channels * num_features, channels, kernel_size=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(channels)
+        )
+        self.predict = nn.Conv2d(channels, num_classes, kernel_size=1)
+
+    def forward(self, features):
+        x = torch.cat(features, dim=1)
+        x = self.fusion(x)
+        x = self.predict(x)
+        return x
+
+class SegFormer(nn.Module):
+    """ SegFormer """
+    def __init__(
+        self,
+        in_channels: int,
+        widths: List[int],
+        depths: List[int],
+        all_num_heads: List[int],
+        patch_sizes: List[int],
+        overlap_sizes: List[int],
+        reduction_ratios: List[int],
+        mlp_expansions: List[int],
+        decoder_channels: int,
+        scale_factors: List[int],
+        num_classes: int,
+        drop_prob: float = 0.0,
+    ):
+        """
+            in_channels: Number of input channels
+            widths: Widths of the encoder stages
+            depths: Depth of each encoder stage
+            all_num_heads: Number of heads in each stage
+            patch_sizes: Patch sizes in each stage
+            overlap_sizes: Overlap sizes in each stage
+            reduction_ratios: Reduction ratios in each stage
+            mlp_expansions: MLP expansion ratios in each stage
+            decoder_channels: Decoder channels
+            scale_factors: Scale factors in the decoder
+            num_classes: Number of classes
+            drop_prob: Dropout probability
+        """
+        super().__init__()
+        self.encoder = Encoder(
+            in_channels,
+            widths,
+            depths,
+            all_num_heads,
+            patch_sizes,
+            overlap_sizes,
+            reduction_ratios,
+            mlp_expansions,
+            drop_prob,
+        )
+        self.decoder = Decoder(decoder_channels, widths[::-1], scale_factors)
+        self.head = SegmentationHead(decoder_channels, num_classes, num_features=len(widths))
+
+    def forward(self, x):
+        features = self.encoder(x)
+        features = self.decoder(features[::-1])
+        segmentation = self.head(features)
 
         # Interpolate to match the size of the input
-        seg_output = torch.nn.functional.interpolate(seg_output, size=x.size()[2:], mode='bilinear', align_corners=False)
+        segmentation = torch.nn.functional.interpolate(segmentation, size=x.size()[2:], mode='bilinear', align_corners=False)
+        return segmentation
 
-        return seg_output
 
-if __name__ == '__main__':
-    # Instantiate the model
-    model = Segformer()
-    print(model)
+segformer = SegFormer(
+    in_channels=3,
+    widths=[64, 128, 256, 512],
+    depths=[3, 4, 6, 3],
+    all_num_heads=[1, 2, 4, 8],
+    patch_sizes=[7, 3, 3, 3],
+    overlap_sizes=[4, 2, 2, 2],
+    reduction_ratios=[8, 4, 2, 1],
+    mlp_expansions=[4, 4, 4, 4],
+    decoder_channels=256,
+    scale_factors=[8, 4, 2, 1],
+    num_classes=1,
+)
 
-    # Create a random tensor of shape (batch_size, channels, height, width)
-    input_tensor = torch.rand(1, 3, 256, 256)
-
-    # Pass the tensor through the model
-    output = model(input_tensor)
-
-    # Check the output shape
-    print(output.shape)
+segmentation = segformer(torch.randn((1, 3, 224, 224)))
+print(segmentation.shape) # torch.Size([1, 1, 224, 224])
